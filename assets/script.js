1116ee9d0eb1cb1e73f1aaf6aaf9b3689cca7752
Tyler Bedard <tylerbedard126@gmail.com>
2022-10-28 22:27:08 -0400

    fixed the styling

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
+
+// variables
 const today = moment();
+const saveButton = $('#saveBtn')
+let currentTime = moment().hours()
+
+
+
+
+
+
+console.log(currentTime)
+
+
+
+// sets the day of the month/year to the page
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-
+if (currentTime === 22) {
+    $('textarea').css("backround-color", "gray");
+}
