c47513d7fbd7e6dde6558d3a802ec7568f0a26e2
Tyler Bedard <tylerbedard126@gmail.com>
2022-10-29 15:04:05 -0400

    changed the html to jquery and added function to change color based on time of day

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,60 @@
 
 // variables
 const today = moment();
-const saveButton = $('#saveBtn')
-let currentTime = moment().hours()
+const saveButton = $('.saveBtn');
+const $container = $('.container');
+const currentHour = today.hour();
 
+console.log(currentHour, 'HOUR');
+// function for creating the calendar
+const createCalendar = () => {
+    const times = [
+        { text: '7AM', military: 7 },
+        { text: '8AM', military: 8 },
+        { text: '9AM', military: 9 },
+        { text: '10AM', military: 10 },
+        { text: '11AM', military: 11 },
+        { text: '12PM', military: 12 },
+        { text: '1PM', military: 13 },
+        { text: '2PM', military: 14 },
+        { text: '3PM', military: 15 },
+    ];
+    times.forEach(obj => {
+        console.log(checkCurrentTime(currentHour, obj.military));
+        const $div = $('<div>').addClass('row time-block');
+        const $div1 = $('<div>').addClass('hour col-1');
+        const $textArea = $('<textarea>').addClass(
+            `description col-10  ${checkCurrentTime(currentHour, obj.military)}`
+        );
+        const $button = $('<button>').addClass('saveBtn col-1 btn').attr('time', obj.military);
+        const $i = $('<i>').addClass('fas fa-save');
 
+        $div.append($div1.text(obj.text), $textArea, $button.append($i));
 
+        return $container.append($div);
+    });
+};
+// function for changing the colors of the text areas based on time of day
+const checkCurrentTime = (currentTime, militaryTime) => {
+    if (currentTime === militaryTime) {
+        return 'present';
+    } else if (currentTime > militaryTime) {
+        return 'past';
+    } else {
+        return 'future';
+    }
+};
 
 function saveData() {
-    console.log("did this work")
+    console.log('did this work');
 }
 
-
-saveButton.click(saveData)
-
+$('button.saveBtn').click(saveData);
 
 // sets the day of the month/year to the page
-$("#currentDay").text(today.format("MMM Do, YYYY"));
-
-// setting the colors to the text areas based on time of day
-if (currentTime === currentTime) {
-    $('textarea').addClass("present");
-}
-else {
-    $('textarea').addClass("past");
-}
+$('#currentDay').text(today.format('MMM Do, YYYY'));
 
 
 
-
-// at the current time, corresponding text area should be red. the time in the past should be white, and
-// the time in  future should be green
-
+// Create divs for calendar
+createCalendar();
