d378239f19e6bcba4ed220dec605b93f226c290e
Tyler Bedard <tylerbedard126@gmail.com>
2022-10-29 12:07:47 -0400

    started the time function

--- assets/script.js
+++ assets/script.js
@@ -7,15 +7,28 @@ let currentTime = moment().hours()
 
 
 
+function saveData() {
+    console.log("did this work")
+}
 
 
-console.log(currentTime)
-
+saveButton.click(saveData)
 
 
 // sets the day of the month/year to the page
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-if (currentTime === 22) {
-    $('textarea').css("backround-color", "gray");
+// setting the colors to the text areas based on time of day
+if (currentTime === currentTime) {
+    $('textarea').addClass("present");
 }
+else {
+    $('textarea').addClass("past");
+}
+
+
+
+
+// at the current time, corresponding text area should be red. the time in the past should be white, and
+// the time in  future should be green
+
